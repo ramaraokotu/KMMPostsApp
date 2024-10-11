72e4bbbcb48ff2f5af175c838dba013ed0c12db9
ramaraokotu <itsupport@itsupports-MacBook-Pro.local>
2024-10-11 23:41:40 +0530

    Setup Koin and other dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -27,13 +27,22 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.bundles.ktor)
+            api(libs.koin.core)
+            implementation(libs.koin.bom)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.lifecycle.viewmodel)
+            implementation(libs.navigation.compose)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
 
         androidMain.dependencies {
-         implementation(libs.ktor.client.okhttp)
+            implementation(libs.ktor.client.okhttp)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
         }
 
         iosMain.dependencies {
